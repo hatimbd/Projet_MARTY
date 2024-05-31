e7cb0f289fda074f705c906fa2eda6d99ef4bc97
WchOussama <148217915+WchOussama@users.noreply.github.com>
2024-05-31 13:44:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -14,6 +14,7 @@ class MartyRobot:
 
     def marcher(self, pas, start_foot, tourner, step_length, temps_mouvement):
         self.marty.walk(pas, start_foot, tourner, step_length, temps_mouvement)
+        self.marty.stand_straight()
 
     def danse(self):
         self.marty.dance()
@@ -156,52 +157,64 @@ class MartyControlApp(QWidget):
     def walk(self, pas):
         if self.marty_robot:
             pas = int(self.pas_input.text())
-            self.marty_robot.marcher(pas, 'auto', 0, 25, 3000)
+            mon_thread = threading.Thread(target = self.marty_robot.marcher, args= (pas, 'auto', 0, 25, 3000))
+            mon_thread.start()
 
     def turn_right(self):
         if self.marty_robot:
-            self.marty_robot.marcher(3, 'auto', -20, 25, 3000)
-            self.marty_robot.se_tenir_droit()
+            mon_thread = threading.Thread(target = self.marty_robot.marcher, args= (3, 'auto', -20, 25, 3000))
+            mon_thread.start()
+            
     
     def turn_left(self):
         if self.marty_robot:
-            self.marty_robot.marcher(3, 'auto', 20, 25, 3000)
-            self.marty_robot.se_tenir_droit()
+            mon_thread = threading.Thread(target = self.marty_robot.marcher, args= (3, 'auto', 20, 25, 3000))
+            mon_thread.start()
+            
 
     def dance(self):
         if self.marty_robot:
-            self.marty_robot.danse()
+            mon_thread = threading.Thread(target = self.marty_robot.danse)
+            mon_thread.start()
 
     def stand_straight(self):
         if self.marty_robot:
-            self.marty_robot.se_tenir_droit()
+            mon_thread = threading.Thread(target = self.marty_robot.se_tenir_droit)
+            mon_thread.start()
 
     def play_sound(self):
         if self.marty_robot:
-            self.marty_robot.jouer_son(1)
+            mon_thread = threading.Thread(target = self.marty_robot.jouer_son, args= (1))
+            mon_thread.start()
+            
 
     def get_distance(self):
         if self.marty_robot:
             distance = self.marty_robot.obtenir_distance()
-            self.status_label.setText(f'Distance: {distance} cm')
+            mon_thread = threading.Thread(target = self.status_label.setText(f'Distance: {distance} cm'))
+            mon_thread.start()
 
     def get_accelerometer(self):
         if self.marty_robot:
             accelerometer = self.marty_robot.obtenir_accelerometre()
-            self.status_label.setText(f'Accéléromètre: {accelerometer}')
+            mon_thread = threading.Thread(target = self.status_label.setText(f'Accéléromètre: {accelerometer}'))
+            mon_thread.start()
 
     def stop(self):
         if self.marty_robot:
-            self.marty_robot.arreter()
+            mon_thread = threading.Thread(target = self.marty_robot.arreter)
+            mon_thread.start()
 
     def high_five(self):
         if self.marty_robot:
-            self.marty_robot.high_five()
-            
+            mon_thread = threading.Thread(target = self.marty_robot.high_five)
+            mon_thread.start()
+
     def get_battery(self):
         if self.marty_robot:
             battery = self.marty_robot.obtenir_niveau_batterie()
-            self.battery_label.setText(f'Niveau de batterie: {battery["percentage"]}%')
+            mon_thread = threading.Thread(target = self.battery_label.setText(f'Niveau de batterie: {battery["percentage"]}%'))
+            mon_thread.start()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
