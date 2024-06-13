6b3d036539fcc9a658d3145142c1bc0072a36b47
Hatim Bahand <148228832+hatimbd@users.noreply.github.com>
2024-06-13 11:45:08 +0200

    Update class_MartyRobot.py

--- Application/Interface/class_MartyRobot.py
+++ Application/Interface/class_MartyRobot.py
@@ -1,10 +1,5 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QHBoxLayout, QGroupBox, QLineEdit
-from PyQt6.QtCore import Qt
-from PyQt6.QtCore import QSize
-from PyQt6.QtGui import QIcon
 from martypy import Marty, MartyConfigException
-import threading
+
 
 class MartyRobot:
     def __init__(self, adresse_ip):
@@ -64,3 +59,59 @@ class MartyRobot:
 
     def bouger_oeil(self, pose_or_angle, move_time=1000, bloquant=None):
         self.marty.eyes(pose_or_angle, move_time, bloquant)
+
+    def get_color_name(self):
+        s=0
+        for i in range(10) :
+            color_sensor_reading = self.marty.get_ground_sensor_reading('LeftColorSensor')
+            s+=color_sensor_reading
+            i+=1
+        s=s/10
+        print(color_sensor_reading)
+        if 14 <= s <= 17 :
+            print("Noir")
+            return "Noir"
+        elif 19 <= s <= 23 : 
+            print("BleuM")
+            return "BleuM"
+        elif 89 <= s <= 99 :
+            print("Rose")
+            return "Rose"
+        elif 27 <= s <= 33:
+            print("Vert")
+            return "Vert"
+        elif 167 <= s <= 193 :
+            print("Jaune")
+            return "Jaune"
+        elif 73 <= s <= 87 :
+            print("Rouge")
+            return "Rouge"
+        elif 49 <= s <= 52 :
+            print("BleuC")
+            return("BleuC")
+        else:
+            print("Couleur inconnue")
+            return "Couleur inconnue"
+    global scan 
+    scan = True    
+    def act_on_color(self):
+        while scan:
+            color = self.get_color_name()
+            if color == "BleuC":
+                self.marty.eyes('wiggle')
+                self.marty.walk(num_steps=6, step_length=30, move_time=2000, blocking=True)
+            if color == "Rouge":
+                self.marty.dance()
+                self.marty.stop()
+            elif color == "Jaune":
+                self.marty.walk(num_steps=4, step_length=-30, move_time=2000, blocking=True)
+            elif color == "Vert":
+                self.marty.walk(num_steps=6, step_length=30, move_time=2000, blocking=True)
+            elif color == "BleuM":
+                self.marty.sidestep('right', steps=6, step_length=35, move_time=2000, blocking=True)
+                self.marty.stand_straight()
+            elif color == "Rose":
+                self.marty.sidestep('left', steps=6, step_length=35, move_time=2000, blocking=True)
+                self.marty.stand_straight()
+            elif color == "noir":
+                self.marty.stop()
